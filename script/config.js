9ecac1ae5a0dfb69f49bc7e105f4e44f4e68f0b7
Eduardo Operacional <operacional@credvix.com>
2025-07-08 16:08:32 -0300

    acerto do scritp de email

--- script/config.js
+++ script/config.js
@@ -1,3 +1,3 @@
 // Configurações
 const googleSheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT2lunjtU5EiVIVkwWBMK574TAoIYKMqWlTAvy1LyD0tvd0yHqTVL6CxkFyGRSk2PVm-N9ezp4aU-Qm/pub?gid=0&single=true&output=csv';
-const googleAppScriptURL = 'https://script.google.com/macros/s/AKfycbz8NiQBapWrxRqITxUysSZZXKowMgMOrQC_yMsF9ezzzuZ3tM4tzp70O3H_N2ZVo7OG/exec';
+const googleAppScriptURL = 'https://script.google.com/macros/s/AKfycbwyzVKK3YTD3e-rnwosmuI3JWRJxc3iKo4FWIsoGIvd-l71sAMufr75ddrI5HC-dkDw/exec';
